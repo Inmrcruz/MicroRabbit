1e74419c15a38a2b5d71d1080443ca98391ad063
Ricky <rcruz78@outlook.com>
2019-10-09 20:50:45 -0400

    Banking Development

--- MicroRabbit.Banking.Api/Startup.cs
+++ MicroRabbit.Banking.Api/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using MediatR;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Hosting;
-using MicroRabbit.Infra.IoC;
-using MicroRabbit.Banking.Data.Context;
-using Microsoft.EntityFrameworkCore;
+//using Swashbuckle.AspNetCore.Swagger;
 
 namespace MicroRabbit.Banking.Api
 {
@@ -29,17 +24,24 @@ namespace MicroRabbit.Banking.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddDbContext<BankingDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
             });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Info { Title = "Banking Microservice", Version = "v1" });
+            //});
+
+            services.AddMediatR(typeof(Startup));
             RegisterServices(services);
+
         }
 
         private void RegisterServices(IServiceCollection services)
             => DependencyContainer.RegisterServices(services);
-
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -48,17 +50,21 @@ namespace MicroRabbit.Banking.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
+            //app.UseHttpsRedirection();
+
             app.UseRouting();
-            app.UseHttpsRedirection();
+
             app.UseAuthorization();
-            app.UseEndpoints(enpoints =>
+            //app.UseSwagger();
+            //app.UseSwaggerUI(c => 
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1");
+            //});
+
+            app.UseEndpoints(endpoints =>
             {
-                enpoints.MapControllers();
+                endpoints.MapControllers();
             });
         }
     }
